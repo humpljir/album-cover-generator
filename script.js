4731a2d813c1b8c0cd6fcbb09f0a013ba79eb48a
Jirka Humpl <jriihumpl@gmail.com>
2021-09-05 18:13:48 +0200

    added styling

---
+++ script.js
@@ -0,0 +1,25 @@
+var can;
+
+function pageInit() {
+    document.getElementById("input-form").addEventListener("onchange", () => {});
+    can = document.getElementById("canvas");
+    
+    var ctx = can.getContext("2d");
+    var img = document.getElementById("template-img");
+    ctx.drawImage(img, 0, 0);
+}
+
+function generateFile(e) {
+      var image = can.toDataURL("image/png").replace("image/png", "image/octet-stream");
+
+      var element = document.createElement('a');
+      var filename = 'test.png';
+      element.setAttribute('href', image);
+      element.setAttribute('download', filename);
+
+      element.click();
+}
+
+window.onload = function () {
+    pageInit();
+};
